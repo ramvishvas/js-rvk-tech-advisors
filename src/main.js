c123012f83463301ee62b9144171761bb260d188
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2024-01-19 12:47:52 +0530

    Add basic three js code

--- src/main.js
+++ src/main.js
@@ -1,1 +1,41 @@
 import './style.css';
+
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+const app = document.getElementById('app');
+
+// Scene
+const scene = new THREE.Scene();
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+app.appendChild(renderer.domElement);
+
+// Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Animate
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+// Check if WebGL is available
+if (WebGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    animate();
+} else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById('app').appendChild(warning);
+}
